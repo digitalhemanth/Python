3a174575ad812399406e62aefffbd0e028d04987
Hemanth Kumar <digitalhemanth@gmail.com>
2022-01-01 23:06:21 +0530

    Update Lambda_map_zip.py

--- Code/Lambda_map_zip.py
+++ Code/Lambda_map_zip.py
@@ -3,3 +3,22 @@
 
 add = lambda a,b : print(a+b)
 add(6,8)
+
+#Map
+#The returned value from map() (map object) then can be passed to functions like list() (to create a list), set() (to create a set) .
+
+nums = [2,8,6,4]
+
+def sqr(n):
+    n = n*n
+    
+output = map(sqr,nums)
+
+print(output)
+
+'''
+Syntax: map(fun, iter)
+fun : It is a function to which map passes each element of given iterable.
+iter : It is a iterable which is to be mapped.
+
+'''
