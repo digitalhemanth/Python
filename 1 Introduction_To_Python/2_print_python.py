d3562e22a7d5c778a926ad6f1707e2717b4a7f30
Hemanth Kumar <digitalhemanth@users.noreply.github.com>
2022-03-03 23:56:16 +0530

    Update 2_print_python.py

--- 1 Introduction_To_Python/2_print_python.py
+++ 1 Introduction_To_Python/2_print_python.py
@@ -7,8 +7,6 @@ print('"Hemanth" is a Devloper come Hacker ')
 bar = '"It\'s awesome" he said.'
 print(bar)
 
-
-
 #Single quotes allow un-escaped embedding of double quotes.
 #Double quotes allow for embedding of un-escaped single quotes.
 
